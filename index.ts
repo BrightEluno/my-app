452199845202743cbf9be1a98d7ac3d2827be5ed
brightchinaza <brightochinaza96@gmail.com>
2025-10-16 00:00:56 +0100

    Set up TailwindCSS

---
+++ index.ts
@@ -0,0 +1,20 @@
+// bun add -d concurrently
+
+import concurrently from 'concurrently'
+
+concurrently([
+    {
+        name: 'server',
+        command: 'bun run dev',
+        cwd: 'packages/server',
+        prefixColor: 'cyan'
+
+    },
+    {
+        name: 'client',
+        command: 'bun run dev',
+        cwd: 'packages/client',
+        prefixColor: 'green'
+
+    }
+])

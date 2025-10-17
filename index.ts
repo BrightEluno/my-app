f95ebdb42a8a71fbbc0ba3d67d9fd81838ebdc11
brightchinaza <brightochinaza96@gmail.com>
2025-10-17 17:11:20 +0100

    Set up Prettier

--- index.ts
+++ index.ts
@@ -1,20 +1,18 @@
 // bun add -d concurrently
 
-import concurrently from 'concurrently'
+import concurrently from 'concurrently';
 
 concurrently([
-    {
-        name: 'server',
-        command: 'bun run dev',
-        cwd: 'packages/server',
-        prefixColor: 'cyan'
-
-    },
-    {
-        name: 'client',
-        command: 'bun run dev',
-        cwd: 'packages/client',
-        prefixColor: 'green'
-
-    }
-])
+   {
+      name: 'server',
+      command: 'bun run dev',
+      cwd: 'packages/server',
+      prefixColor: 'cyan',
+   },
+   {
+      name: 'client',
+      command: 'bun run dev',
+      cwd: 'packages/client',
+      prefixColor: 'green',
+   },
+]);
--- packages/server/index.ts
+++ packages/server/index.ts
@@ -1,20 +1,19 @@
-import express  from "express";
-import type {Request, Response} from "express"
-import dotenv from "dotenv"
+import express from 'express';
+import type { Request, Response } from 'express';
+import dotenv from 'dotenv';
 
 dotenv.config();
 
 const app = express();
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 3000;
 
 app.get('/', (req: Request, res: Response) => {
-    res.send("Hello world");
-})
+   res.send('Hello world');
+});
 app.get('/api/hello', (req: Request, res: Response) => {
-    res.json({message: "Hello World"});
-})
+   res.json({ message: 'Hello World' });
+});
 
-app.listen(port, ()=> {
-    console.log(`Server is running on http://localhost:${port} `);
-    
-})
+app.listen(port, () => {
+   console.log(`Server is running on http://localhost:${port} `);
+});
